804bf315adbe73ef2d15682fdd5df07c4f05f927
bedirhandogan <bedirhandgn14@gmail.com>
2023-10-07 18:14:16 +0300

    test: write tests newColor & useNewColor functions

--- color_test.go
+++ color_test.go
@@ -3,6 +3,7 @@ package color
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -27,5 +28,55 @@ func TestColorize(t *testing.T) {
 
 	for _, s := range scenarios {
 		asrt.Equal(Colorize(s.input), s.output)
+	}
+}
+
+func TestNewColor(t *testing.T) {
+	asrt := assert.New(t)
+
+	scenarios := []struct {
+		name  string
+		value []int
+	}{
+		{
+			name:  "TestRed",
+			value: []int{255, 0, 0},
+		}, {
+			name:  "TestGreen",
+			value: []int{10},
+		},
+	}
+
+	for _, s := range scenarios {
+		// create new color
+		NewColor(s.name, s.value...)
+
+		_, exist := newColors[strings.ToLower(s.name)]
+		asrt.Equal(exist, true)
+	}
+}
+
+func TestUseNewColor(t *testing.T) {
+	asrt := assert.New(t)
+
+	NewColor("TestRed", 9)
+	NewColor("BgTestRed", 9)
+
+	scenarios := []struct {
+		text, match, output string
+	}{
+		{
+			text:   "%TestRed Hello, World!",
+			match:  "%TestRed",
+			output: fmt.Sprintf("\x1b[38;5;%dmHello, World!", 9),
+		}, {
+			text:   "%BgTestRed Hello, World!",
+			match:  "%BgTestRed",
+			output: fmt.Sprintf("\x1b[48;5;%dmHello, World!", 9),
+		},
+	}
+
+	for _, s := range scenarios {
+		asrt.Equal(useNewColor(s.text, s.match), s.output)
 	}
 }
