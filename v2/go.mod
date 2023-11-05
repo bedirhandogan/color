16f9af156fb3f1b4b48f6c456886036dae58a895
bedirhandogan <bedirhandgn14@gmail.com>
2023-11-05 14:00:17 +0300

    refactor: if the terminal supports RGB, use RGB

--- v2/go.mod
+++ v2/go.mod
@@ -2,10 +2,15 @@ module github.com/bedirhandogan/color/v2
 
 go 1.21.3
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/jwalton/go-supportscolor v1.2.0
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- v2/types.go
+++ v2/types.go
@@ -1,6 +1,6 @@
 package color
 
-import "os"
+import "github.com/jwalton/go-supportscolor"
 
 // The 'color' interface defines the necessary method for color types.
 type color interface {
@@ -24,17 +24,17 @@ type rgb struct {
 	b uint8
 }
 
-// The escape method for the 'rgb' struct generates the appropriate escape sequence
-// based on the RGB color components. It checks the COLORTERM environment variable
-// to determine if the terminal supports true color (24-bit color). If true color
-// is supported, it uses the 'RgbToEscape' function directly. Otherwise, it converts
-// the RGB values to ANSI color using 'RgbToAnsi' and then generates the escape sequence.
+// The escape method takes an `rgb` object representing the given RGB color values
+// and is used to produce colored output for the standard error stream (`stderr`) based
+// on the supported color features. If stderr supports 16 million colors, it converts
+// the RGB color values to an escape sequence; otherwise, it converts the RGB color
+// values to ANSI color codes and then into an escape sequence to generate colored output.
+// The `back` parameter determines whether the color will be used as a background (true)
+// or foreground (false) color.
 func (c *rgb) escape(back bool) string {
-	colorterm := os.Getenv("COLORTERM")
-
-	if colorterm == "truecolor" || colorterm == "24bit" {
-		return RgbToEscape(c.r, c.b, c.g, back)
+	if supportscolor.Stderr().Has16m {
+		return RgbToEscape(c.r, c.g, c.b, back)
 	}
 
-	return AnsiToEscape(RgbToAnsi(c.r, c.b, c.g), back)
+	return AnsiToEscape(RgbToAnsi(c.r, c.g, c.b), back)
 }
